25a8c58190929d18204fc7fb42c058cb0438af9f
Evelyn <evelynjohana.94@gmail.com>
2021-02-01 20:13:53 +0100

    add .container y ad h3
    rectificación del formulario

--- src/components/formulario.vue
+++ src/components/formulario.vue
@@ -1,50 +1,53 @@
 <template>
   <div id="formulario">
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputName">Nombre</label>
-        <input type="name" class="form-control" id="inputName" placeholder="Name">
+    <div class="container">
+      <h3>Contactanos</h3>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputName">Nombre</label>
+            <input type="name" class="form-control" id="inputName" placeholder="Nombre">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputUsername">Apellido</label>
+            <input type="Username" class="form-control" id="inputUsername" placeholder="Apellido">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputEmail4">Email</label>
+            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputTelefono">Teléfono</label>
+            <input type="telefono" class="form-control" id="inputTelefono" placeholder="Telefono">
+          </div>
       </div>
-      <div class="form-group col-md-6">
-        <label for="inputUsername">Apellido</label>
-        <input type="Username" class="form-control" id="inputUsername" placeholder="Username">
+      <div class="form-group">
+        <label for="inputAddress">Dirección</label>
+        <input type="text" class="form-control" id="inputAddress" placeholder="av.palau 123">
       </div>
-      <div class="form-group col-md-6">
-        <label for="inputEmail4">Email</label>
-        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+      <div class="form-row">
+        <div class="form-group col-md-4">
+          <label for="inputCity">Ciudad</label>
+          <select id="inputCity" class="form-control">
+            <option value="1">Madrid</option>
+            <option value="2">Barcelona</option>
+            <option value="3">Valencia</option>
+          </select>
+        </div>
+        <div class="form-group col-md-2">
+          <label for="inputZip">CP</label>
+          <input type="text" class="form-control" id="inputZip">
+        </div>
       </div>
-      <div class="form-group col-md-6">
-        <label for="inputTelefono">Teléfono</label>
-        <input type="telefono" class="form-control" id="inputTelefono" placeholder="Telefono">
+      <div class="form-group">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck">
+          <label class="form-check-label" for="gridCheck">
+            Acepto las condiciones
+          </label>
+        </div>
       </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Dirección</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-4">
-      <label for="inputCity">Ciudad</label>
-      <select id="inputCity" class="form-control">
-        <option value="1">Madrid</option>
-        <option value="2">Barcelona</option>
-        <option value="3">Valencia</option>
-      </select>
+      <button type="submit" class="btn btn-primary">ENVIAR</button>
     </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">CP</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">ENVIAR</button>
   </div>
 </template>
 
@@ -55,8 +58,19 @@ export default {
 }
 </script>
 <style scoped>
-#formulario{
+h3{
+  padding: 18px;
+}
+#formaulario{
   background: pink;
   height: 500px;
+  width: 100%;
 }
+.container{
+  background: white;
+  height: 500px;
+  width: 500px;
+
+}
+
 </style>
